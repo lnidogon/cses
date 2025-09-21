177be3dc7383b17524dff46c222df0eb1fa4c48c
lnidogon <lovro.nidogon@gmail.com>
2025-09-21 02:43:18 +0200

    Distinct Value Subarrays

--- distinct_values_subarrays.cpp
+++ distinct_values_subarrays.cpp
@@ -3,36 +3,45 @@
 using namespace std;
 void solve() {
     int n;
-    vector<int> v(n);
     set<int> s;
     map<int, vector<int> > m;
     cin >> n;
+    vector<int> v;
     s.insert(n);
     for(int i = 0; i < n; i++) {
         int x;
         cin >> x;
         v.push_back(x);
+        if(m.find(x) == m.end()) {
+            vector<int> temp;
+            m[x] = temp;
+        }
         m[x].push_back(i);
     }
     for(auto t:m) {
-        reverse(t.second.begin(), t.second.end());
+        reverse(m[t.first].begin(), m[t.first].end());
     }
-    for(auto t:m) {
-        t.second.pop_back();
-        if(t.second.size()) s.insert(t.second.back());
+    for(auto t:m) { 
+        m[t.first].pop_back();
+        if(m[t.first].size()) s.insert(m[t.first].back());
     }
     long long ans = 0;
-    for(int i = 0; i < n;i ++) {
+    for(int i = 0; i < n; i ++) {
         int mn = *s.begin();
         ans += mn - i;
         int x = v[i];
-        s.erase(m[x].back());
-        m[x].pop_back();
-        if(m[x].size())s.insert(m[x].back());
+        if(s.find(m[x].back()) != s.end()) {
+            s.erase(m[x].back());
+        }
+        if(m[x].size()) m[x].pop_back();
+        if(m[x].size()) {
+            s.insert(m[x].back());
+        }
     }
-    cout << ans;
+    cout << ans << '\n';
 
 }
 int main() {
+    ios_base::sync_with_stdio(false);
     solve();
 }
